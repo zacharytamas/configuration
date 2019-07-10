14d3d4c015d77ce90bf33d47e8f5ec13db26fc9f
Zachary Jones <zachary@jones.cx>
2019-07-10 12:42:25 -0400

    Enhancements to `npm.fish`
    I am not intending (at this moment) to be entirely exhaustive with this,
    instead focusing on just the commands I mess up with most.

--- .config/fish/functions/npm.fish
+++ .config/fish/functions/npm.fish
@@ -1,21 +1,22 @@
 function npm --description "Allows using npm-cli args even in Yarn projects"
   # TODO Support discovery of lock files in paths above `cwd`.
 
-  if test -f "package-lock.json"
-    echo "Using `npm`..."
-    # This project is using npm so we can just pass along all the arguments
-    command npm $argv
-  else if test -f "yarn.lock"
+  if test -f "yarn.lock"
     echo "Using `yarn`..."
 
     switch $argv[1]
       case i install
-        # TODO: Detect if the intent is to add a package or if to just
         yarn install
+      case "test"
+        yarn run test
+      case start
+        yarn run start
       case '*'
         echo "Not supported, you'll need to use the actual yarn command."
     end
-
+  else
+    echo "Using `npm`..."
+    # This project is using npm so we can just pass along all the arguments
+    command npm $argv
   end
-
 end
